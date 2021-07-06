6a85723f5c1c3c60144dccfae8b1534277c39267
alina n <ananestean@shift7digital.com>
2021-07-06 18:30:57 +0300

    first commit

---
+++ src/data/baby-data.js
@@ -0,0 +1,19 @@
+import M2021226 from "../assets/resources/cards/2021.2.26 multumire.png";
+import ziDeNastereData from "./zi-de-nastere-data";
+
+const sarcinaBebelusData = [
+    {
+        image: M2021226,
+        imgAlt: "Img Alt"
+    },
+    {
+        image: M2021226,
+        imgAlt: "Img Alt"
+    },
+    {
+        image: M2021226,
+        imgAlt: "Img Alt"
+    }
+];
+
+export default sarcinaBebelusData;
---
+++ src/data/birthday-data.js
@@ -0,0 +1,18 @@
+import M2021226 from "../assets/resources/cards/2021.2.26 multumire.png";
+
+const ziDeNastereData = [
+    {
+        image: M2021226,
+        imgAlt: "Img Alt"
+    },
+    {
+        image: M2021226,
+        imgAlt: "Img Alt"
+    },
+    {
+        image: M2021226,
+        imgAlt: "Img Alt"
+    }
+];
+
+export default ziDeNastereData;
